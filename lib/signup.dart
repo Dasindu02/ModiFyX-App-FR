87778eac3a1e74ee935497d130951f2ebfe3b223
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2026-01-08 20:13:48 +0530

    update the signup

---
+++ lib/signup.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/welcome.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          // Back button
+          Positioned(
+            top: 40,
+            left: 16,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
