3d4d81ce61800358c9288fa7544010e5f61f1d02
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2026-01-10 19:43:36 +0530

    update width

--- lib/signup.dart
+++ lib/signup.dart
@@ -19,7 +19,34 @@ class SignUpPage extends StatelessWidget {
           ),
 
           // Back button
-          Positioned(
+         
+          // Bottom Card
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+             width: double.infinity, 
+                            height: 425,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.65),
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: const [
+                    Text(
+                      'Create Account',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+             Positioned(
             top: 40,
             left: 16,
             child: IconButton(
@@ -28,32 +55,7 @@ class SignUpPage extends StatelessWidget {
             ),
           ),
 
-          // Bottom Card
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              padding: const EdgeInsets.all(24),
-              height: 425,
-              width: 500,
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.65),
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(30),
-                ),
-              ),
-              child: Column(
-                children: const [
-                  Text(
-                    'Create Account',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+
         ],
       ),
     );
