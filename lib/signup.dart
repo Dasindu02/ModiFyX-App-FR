cd10247d1201c60f1397b8701ac36451aea260e1
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2026-01-10 19:33:33 +0530

    update the frontned

--- lib/signup.dart
+++ lib/signup.dart
@@ -8,7 +8,7 @@ class SignUpPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-
+          // Background image
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
@@ -18,12 +18,40 @@ class SignUpPage extends StatelessWidget {
             ),
           ),
 
+          // Back button
           Positioned(
             top: 40,
             left: 16,
             child: IconButton(
               icon: const Icon(Icons.arrow_back, color: Colors.white),
               onPressed: () => Navigator.pop(context),
+            ),
+          ),
+
+          // Bottom Card
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              height: 425,
+              width: 500,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.65),
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: const [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
