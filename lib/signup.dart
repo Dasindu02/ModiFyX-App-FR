d102207fca29e4106c3568c9db175e5a4cb0ee01
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2026-01-13 19:53:35 +0530

    add filed

--- lib/signup.dart
+++ lib/signup.dart
@@ -110,6 +110,31 @@ class SignUpPage extends StatelessWidget {
                       ),
                     ),
                   ),
+
+                   const SizedBox(height: 16),
+
+                  // Password
+                  TextField(
+                    style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+              ),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      hintText: 'Enter confirm Password',
+                       floatingLabelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+
+                      filled: true,
+                      fillColor: Colors.grey.shade200,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
                         
 
                   ],
