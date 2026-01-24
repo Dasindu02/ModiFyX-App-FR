01beb3381721d4ff6f7897433f599028b472de3f
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2026-01-24 23:42:49 +0530

    add the route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { register , login  } from "../controllers/authController.js";
 import { body } from "express-validator";
 
 const router = express.Router();
@@ -13,5 +13,8 @@ router.post(
   ],
   register
 );
+
+router.post("/login", login);
+
 
 export default router;
