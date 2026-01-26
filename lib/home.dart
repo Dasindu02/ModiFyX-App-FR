7f5b0019ad0df3368a07273a44ecbae711c63df7
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2026-01-26 22:26:23 +0530

    Add the greeting with name

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final String fullName;
+
+  const HomePage({super.key, required this.fullName});
+
+  String getGreeting() {
+    final hour = DateTime.now().hour;
+
+    if (hour < 12) {
+      return "Good Morning";
+    } else if (hour < 17) {
+      return "Good Afternoon";
+    } else {
+      return "Good Evening";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Get first name only
+    final firstName = fullName.split(" ").first;
+
     return Scaffold(
-      appBar: AppBar(
-        // title: const Text("Home"),
-      ),
-      body: const Center(
+      appBar: AppBar(),
+      body: Center(
         child: Text(
-          "Login Successful Testing",
-          style: TextStyle(fontSize: 20),
+          "${getGreeting()}, $firstName 👋",
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
